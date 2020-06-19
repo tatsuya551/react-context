ae31a8ac3477a1a0df29d3ea3ffed7607d918b8d
Seki Tatsuya <seki0515ya@gmail.com>
2020-06-19 13:11:58 +0900

    create counter function

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CounterContext from './contexts/counter';
+import Counter from './components/counter';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.refresh = this.refresh.bind(this)
+
+    this.state = {
+      count: 0,
+      increment: this.increment,
+      decrement: this.decrement,
+      refresh: this.refresh
+    }
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 })
+  }
+  refresh() {
+    this.setState({ count: 0 })
+  }
+
+  render() {
+    return(
+      <CounterContext.Provider value={this.state}>
+        <Counter />
+      </CounterContext.Provider>
+    )
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +45,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
